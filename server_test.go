829842d9976ea7cc65d9c309a0838423d6e01027
MrShanks <simonestaffoli@gmail.com>
2024-06-06 10:32:36 +0200

    feat: add different path to Server API /league and /players

--- server_test.go
+++ server_test.go
@@ -7,15 +7,23 @@ import (
 	"testing"
 )
 
+// StubPlayerStore is a mock player store that implements the PlayerStore interface.
+// By doing this, we can write an ad hoc implementation of the playerstore that for
+// example will not need a database running in order to fetch the data needed to run
+// the tests.
 type StubPlayerStore struct {
 	scores   map[string]int
 	winCalls []string
 }
 
+// GetPlayerScore retrieves the player's passed score from the scores map of the
+// StubPlayerStore
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
 	return s.scores[name]
 }
 
+// RecordWin adds the winnners name to the array of the winCalls, by doing that we
+// can measure the length of the winCalls array to know how many time it has been called
 func (s *StubPlayerStore) RecordWin(name string) {
 	s.winCalls = append(s.winCalls, name)
 }
@@ -28,7 +36,7 @@ func TestGETPlayers(t *testing.T) {
 		},
 		[]string{},
 	}
-	server := &PlayerServer{store}
+	server := NewPlayerServer(store)
 
 	t.Run("returns Gino's score", func(t *testing.T) {
 		request := newGetScoreRequest("Gino")
@@ -70,7 +78,7 @@ func TestStoreWins(t *testing.T) {
 		map[string]int{},
 		[]string{},
 	}
-	server := &PlayerServer{store}
+	server := NewPlayerServer(store)
 	t.Run("records wins when POST", func(t *testing.T) {
 		player := "Gianni"
 
@@ -88,6 +96,20 @@ func TestStoreWins(t *testing.T) {
 		if store.winCalls[0] != player {
 			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
 		}
+	})
+}
+
+func TestLeague(t *testing.T) {
+	store := &StubPlayerStore{}
+	server := NewPlayerServer(store)
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
 	})
 }
 
