ef909a16ed9305577a961bab7c39ce0545e9943a
MrShanks <simonestaffoli@gmail.com>
2024-06-09 10:40:51 +0200

    feat: separate the what we write concern from the actual writing

---
+++ tape_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"testing"
+)
+
+func TestTapeWrite(t *testing.T) {
+	file, cleanFile := createTempFile(t, "12345")
+	defer cleanFile()
+
+	tape := &tape{file}
+
+	tape.Write([]byte("abc"))
+
+	file.Seek(0, io.SeekStart)
+	newFileContents, _ := io.ReadAll(file)
+
+	got := string(newFileContents)
+	want := "abc"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
